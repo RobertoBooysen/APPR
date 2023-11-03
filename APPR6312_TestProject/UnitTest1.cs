43b2f898a46eb4db706fdad4f960180faa0c5b5e
RobertoBooysen <101563323+RobertoBooysen@users.noreply.github.com>
2023-11-03 12:32:58 +0200

    Added a unit test for the AllocateGoods method within the AllocateGoodsController class. The primary goal with this test is to ensure that goods allocation behaves correctly by not exceeding the available goods inventory. In the "Arrange" phase, I've set up the necessary context for the test. I've created an instance of the AllocateGoodsController, defined the available goods inventory (100), and specified the goods allocation amount (20). Then, I've called the controller's AllocatingGoods method with a properly configured TblAllocateGoods object. In the"Act" phase, the AllocatingGoods method is invoked and captured its result, which is cast as an IActionResult. In the "Assert" section there is several checks to ensure that the result is not null using Assert.IsNotNull. If the result is a ViewResult, I then inspect the ModelState for any errors related to allocation. As well as confirming that the allocated goods amount doesn't surpass the available inventory, preventing overallocation.

--- APPR6312_TestProject/UnitTest1.cs
+++ APPR6312_TestProject/UnitTest1.cs
@@ -1,13 +1,49 @@
+using APPR6312_POE_Web_Application.Controllers;
+using APPR6312_POE_Web_Application.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 
 namespace APPR6312_TestProject
 {
     [TestClass]
     public class UnitTest1
     {
+        //AllocateGoodsTest verifies goods allocation doesn't exceed available goods (The IIE, 2022)
         [TestMethod]
-        public void TestMethod1()
+        public void AllocateGoodsTest()
         {
+            //Arrange (The IIE, 2022)
+            var controller = new AllocateGoodsController();
+            var availableGoods = 100; //Available goods (The IIE, 2022)
+            var goodsAllocationAmount = 20; //Amount of goods items to allocate (The IIE, 2022)
+
+            var allocateGoods = new TblAllocateGoods
+            {
+                Disasters = "Hunger",
+                Goods = "Canned Food",
+                NumberOfGoods = goodsAllocationAmount,
+            };
+
+            //Act (The IIE, 2022)
+            var result = controller.AllocatingGoods(allocateGoods) as IActionResult;
+
+            //Assert (The IIE, 2022)
+            Assert.IsNotNull(result);
+
+            if (result is ViewResult viewResult)
+            {
+                //Checking for model state errors (The IIE, 2022)
+                var modelState = viewResult.ViewData.ModelState;
+                if (modelState.ContainsKey(string.Empty))
+                {
+                    //Checking if there's an error related to allocation amount (The IIE, 2022)
+                    Assert.IsTrue(modelState[string.Empty].Errors.Any(e => e.ErrorMessage.Contains("Not enough items available in goods donations")));
+                }
+            }
+
+            //Verifing that the goods allocation amount is not greater than the available goods inventory (The IIE, 2022)
+            Assert.IsTrue(goodsAllocationAmount <= availableGoods);
         }
     }
 }
