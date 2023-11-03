09c4304e7df0a500e9881fe2e771cce8464be7c7
RobertoBooysen <101563323+RobertoBooysen@users.noreply.github.com>
2023-11-03 12:42:20 +0200

    Added a unit test for the AllocateMoney method within the AllocateMoneyController class. The primary goal with this test is to ensure that money allocation behaves correctly by not exceeding the available money. In the "Arrange" phase, I've set up the necessary context for the test. I've created an instance of the AllocateMoneyController, defined the available money (10000), and specified the money allocation amount (2000). Then, I've called the controller's AllocatingMoney's method with a properly configured TblAllocateMoney object. In the"Act" phase, the AllocatingMoney method is invoked and captured its result, which is cast as an IActionResult. In the "Assert" section there is several checks to ensure that the result is not null using Assert.IsNotNull. If the result is a ViewResult, I then inspect the ModelState for any errors related to allocation. As well as confirming that the allocated money amount doesn't surpass the available money, preventing overallocation.

--- APPR6312_TestProject/UnitTest1.cs
+++ APPR6312_TestProject/UnitTest1.cs
@@ -45,5 +45,38 @@ namespace APPR6312_TestProject
             //Verifing that the goods allocation amount is not greater than the available goods inventory (The IIE, 2022)
             Assert.IsTrue(goodsAllocationAmount <= availableGoods);
         }
+        //AllocateMoneyTest verifies money allocation doesn't exceed available money (The IIE, 2022)
+        [TestMethod]
+        public void AllocateMoneyTest()
+        {
+            //Arrange (The IIE, 2022)
+            var controller = new AllocateMoneyController();
+            var availableMoney = 10000; //Available money (The IIE, 2022)
+            var allocationAmount = 2000; //Amount to allocate (The IIE, 2022)
+
+            var money = new TblAllocateMoney
+            {
+                Disasters = "Hunger",
+                Amount = allocationAmount,
+            };
+
+            //Act (The IIE, 2022)
+            var result = controller.AddMoney(money) as IActionResult;
+
+            //Assert (The IIE, 2022)
+            Assert.IsNotNull(result);
+
+            if (result is ContentResult contentResult)
+            {
+                //Checking if the response content contains the specific error message as a string (The IIE, 2022)
+                var responseContent = contentResult.Content as string;
+
+                //Verifing that the response content contains the error message (The IIE, 2022)
+                Assert.IsTrue(responseContent.Contains("Not enough money available for allocation"));
+            }
+
+            //Verifing that the allocation amount is not greater than the available money (The IIE, 2022)
+            Assert.IsTrue(allocationAmount < availableMoney);
+        }
     }
 }
