c7a93ca1f01f12303f0c98b7255f9e8879fa7b5f
RobertoBooysen <101563323+RobertoBooysen@users.noreply.github.com>
2023-11-03 12:13:49 +0200

    Added a new proect to the solution which I named APPR6312_TestProject. This project will consist of testing methods for some of the methods in the APPR_6312_POE_Web_Application controllers.

---
+++ APPR6312_TestProject/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace APPR6312_TestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
